b119bb1e6fd16b896a254897dec9ae20de3bddd5
Omar Ali <110394937+om4rrr@users.noreply.github.com>
2026-01-12 22:59:57 +0200

    Refactor segment tree implementation

--- dataStructures/segmentTree.cpp
+++ dataStructures/segmentTree.cpp
@@ -1,45 +1,37 @@
-struct Node
-{
+const int OO = 1'000'000'000;
+
+struct Node {
     int val;
 
-    Node()
-    {
-        val = 0;
+    Node() {
+        val = OO;
     }
 
-    Node(int x)
-    {
+    Node(int x) {
         val = x;
     }
 };
 
 
-struct segTree
-{
+struct segTree {
     vector<Node>segData;
     int treeSize;
-    segTree(int n)
-    {
+    segTree(int n) {
         treeSize = 1;
         while(treeSize < n)
             treeSize *= 2;
         segData.assign(2 * treeSize, Node());
     }
 
-    Node merge(Node & lf, Node & ri)
-    {
+    Node merge(Node & lf, Node & ri) {
         Node ans = Node();
-        ans.val = lf.val + ri.val;
+        ans.val = min(lf.val, ri.val);
         return ans;
     }
 
-    void init(vector<int> &num, int nx, int lx, int rx) // 0 - indexed
-    {
-
-        if(rx - lx == 1)
-        {
-            if(lx < num.size())
-                segData[nx] = Node(num[lx]);
+    void init(vector<int> &num, int nx, int lx, int rx) {
+        if(rx - lx == 1) {
+            if(lx < num.size()) segData[nx] = Node(num[lx]);
             return;
         }
 
@@ -50,57 +42,43 @@ struct segTree
         segData[nx] = merge(segData[2 * nx + 1], segData[2 * nx + 2]);
     }
 
-    void init(vector<int> & num)
-    {
+    void init(vector<int> & num) {
         init(num, 0, 0, treeSize);
     }
 
-    void set(int idx, int val, int nx, int lx, int rx) // 0 - indexed
-    {
-
-        if(rx - lx == 1)
-        {
+    void upd(int idx, int val, int nx, int lx, int rx) {
+        if(rx - lx == 1) {
             segData[nx] = Node(val);
             return;
         }
 
         int mid = (lx + rx) / 2;
-
-        if(idx < mid)
-        {
-            set(idx, val, 2 * nx + 1, lx, mid);
+        if(idx < mid) {
+            upd(idx, val, 2 * nx + 1, lx, mid);
         }
-        else
-        {
-            set(idx, val, 2 * nx + 2, mid, rx);
+        else {
+            upd(idx, val, 2 * nx + 2, mid, rx);
         }
 
         segData[nx] = merge(segData[2 * nx + 1], segData[2 * nx + 2]);
     }
 
-    void set(int idx, int val)
-    {
-        set(idx, val, 0, 0, treeSize);
+    void upd(int idx, int val) {
+        upd(idx, val, 0, 0, treeSize);
     }
 
-    Node get(int l, int r, int nx, int lx, int rx) // 0 - indexed && r not included
-    {
-        if(lx >= l && rx <= r)
-            return segData[nx];
-
-        if(lx >= r || rx <= l)
-            return Node();
+    Node query(int l, int r, int nx, int lx, int rx) {
+        if(lx >= l && rx <= r) return segData[nx];
+        if(lx >= r || rx <= l) return Node();
 
         int mid = (lx + rx) / 2;
-
-        Node lf = get(l, r, 2 * nx + 1, lx, mid);
-        Node ri = get(l, r, 2 * nx + 2, mid, rx);
+        Node lf = query(l, r, 2 * nx + 1, lx, mid);
+        Node ri = query(l, r, 2 * nx + 2, mid, rx);
 
         return merge(lf, ri);
     }
 
-    Node get(int l, int r)
-    {
-        return get(l, r, 0, 0, treeSize);
+    Node query(int l, int r) {
+        return query(l, r, 0, 0, treeSize);
     }
 };
