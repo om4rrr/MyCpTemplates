1a2a65a1089407a14bc0f1e5d9a39e86fe67ca83
om4rrr <omar.2015.oa651@gmail.com>
2026-05-21 03:28:10 +0300

    Update LCA.cpp

--- Trees/LCA.cpp
+++ Trees/LCA.cpp
@@ -1,75 +1,80 @@
-const int N = 1e3 + 5;
+struct LCA {
+  int n;
+  int lg;
+  vector<vector<int>> adj;
+  vector<int> parent;
+  vector<int> depth;
+  vector<vector<int>> jump;
 
-int n;
-vector<int> adj[N];
-int parent[N], depth[N];
-vector<vector<int>> jump;
+  LCA(int n, vector<vector<int>> adj) {
+    this->n = n;
+    this->adj = adj;
+    this->lg = __lg(n) + 2;
 
-void dfs(int u, int p, int dep) {
+    parent.assign(n + 1, 0);
+    depth.assign(n + 1, 0);
+    jump.assign(lg, vector<int>(n + 1, -1));
+
+    build();
+  }
+
+  void dfs(int u, int p, int dep) {
     parent[u] = p;
     depth[u] = dep;
-    for (int &v: adj[u]) {
-        if (v != p)
-            dfs(v, u, dep + 1);
+    for (int &v : adj[u]) {
+      if (v != p)
+        dfs(v, u, dep + 1);
     }
-}
+  }
 
-void build() {
-
-    dfs(1, -1, 0);
-
-    jump = vector<vector<int>>(__lg(n) + 1, vector<int>(n + 1));
+  void build(int root = 1) {
+    dfs(root, 0, 0);
 
     for (int i = 1; i <= n; ++i) {
-        jump[0][i] = parent[i];
+      jump[0][i] = parent[i];
     }
 
-    for (int msk = 1; (1 << msk) <= n; ++msk) {
-        for (int i = 1; i <= n; ++i) {
-            if (jump[msk - 1][i] == -1)
-                jump[msk][i] = -1;
-            else
-                jump[msk][i] = jump[msk - 1][jump[msk - 1][i]];
-        }
+    for (int msk = 1; msk < lg; ++msk) {
+      for (int i = 1; i <= n; ++i) {
+        if (jump[msk - 1][i] == -1)
+          jump[msk][i] = -1;
+        else
+          jump[msk][i] = jump[msk - 1][jump[msk - 1][i]];
+      }
     }
-}
+  }
 
-int get_kth_ancestor(int u, int k) {
-
-    for (int i = __lg(n) + 1; i >= 0; --i) {
-        if ((k >> i) & 1) {
-            if (jump[i][u] == -1)
-                return -1;
-            u = jump[i][u];
-        }
+  int get_kth_ancestor(int u, int k) {
+    for (int i = lg - 1; i >= 0; --i) {
+      if ((k >> i) & 1) {
+        if (u == -1)
+          return -1;
+        u = jump[i][u];
+      }
     }
     return u;
-}
+  }
 
-
-int get_lca(int u, int v) {
+  int get_lca(int u, int v) {
     if (depth[v] > depth[u])
-        swap(u, v);
-    int k = depth[u] - depth[v];
+      swap(u, v);
 
+    int k = depth[u] - depth[v];
     u = get_kth_ancestor(u, k);
 
     if (u == v)
-        return u;
+      return u;
 
-    for (int i = __lg(n); i >= 0; --i) {
-        if (jump[i][v] != jump[i][u]) {
-            u = jump[i][u];
-            v = jump[i][v];
-        }
+    for (int i = lg - 1; i >= 0; --i) {
+      if (jump[i][v] != jump[i][u]) {
+        u = jump[i][u];
+        v = jump[i][v];
+      }
     }
     return jump[0][u];
-}
+  }
 
-void CLR() {
-    for (int i = 1; i < N; ++i) {
-        adj[i].clear();
-        parent[i] = -1;
-        depth[i] = 0;
-    }
-}
+  int get_distance(int u, int v) {
+    return depth[u] + depth[v] - 2 * depth[get_lca(u, v)];
+  }
+};
